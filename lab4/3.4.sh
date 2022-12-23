dddb7e6ad24946820f62d8ecff7e16af7d9a629e
IvanSt1 <70325530+IvanSt1@users.noreply.github.com>
2022-12-23 13:11:37 +0300

    Add files via upload

--- lab4/3.4.sh
+++ lab4/3.4.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 echo "Каталоги:"
-ls -l | grep ^d
+ls -l "$1" | grep ^d
 echo "Обычные файлы:"
-ls -l | grep ^-
+ls -l "$1" | grep ^-
 echo "Символьные ссылки:"
-ls -l | grep ^l
+ls -l "$1" | grep ^l
 echo "Символьные устройства:"
-ls -l | grep ^c
+ls -l "$1" | grep ^c
 echo "Блочные устройства:"
-ls -l | grep ^b
+ls -l "$1" | grep ^b
